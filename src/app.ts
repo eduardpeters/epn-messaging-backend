fbc6ecb5463da6265f0ad476281c61e03d539e11
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-04-15 11:00:17 +0200

    Add basic socket connection

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,38 @@
 import * as dotenv from 'dotenv';
+import cors from 'cors';
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CORS_ORIGIN
+    }
 });
 
-app.listen(PORT, () => {
-  return console.log(`Express is listening at http://localhost:${PORT}`);
+io.on('connection', (socket) => {
+    console.log(`${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+    socket.on("connect_error", (err) => {
+        console.log(`connect_error due to ${err.message}`);
+    });
+});
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello World!',
+    });
+});
+
+server.listen(PORT, () => {
+    return console.log(`Up and running! Listening on port: ${PORT}`);
 });

85eee6a4087bd471911472dacc3c42586398cebe
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-06-11 13:16:42 +0200

    Add pg client to project

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
+import { query } from '../db/query';
 
 async function registerUser(req: Request, res: Response) {
+    const queryString = "SELECT * FROM users;"
+    const result = await query(queryString);
+    console.log(result);
     return res.status(201).send("Registered!");
 }
 
